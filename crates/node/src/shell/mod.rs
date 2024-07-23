5ead8c281edab0a2c340d29ce39ab629c5bf8b92
Tomáš Zemanovič <tomas@heliax.dev>
2024-07-23 20:41:21 +0100

    add conditional fuzzing sig acceptance at lower level

--- crates/node/src/shell/mod.rs
+++ crates/node/src/shell/mod.rs
@@ -1034,7 +1034,6 @@ where
         }
 
         // Tx signature check
-        #[cfg(not(fuzzing))]
         let tx_type = match tx.validate_tx() {
             Ok(_) => tx.header(),
             Err(msg) => {
@@ -1043,8 +1042,6 @@ where
                 return response;
             }
         };
-        #[cfg(fuzzing)]
-        let tx_type = tx.header();
 
         // try to parse a vote extension protocol tx from
         // the provided tx data
